b292e7abd3ff6e16b188658ad20124a574d88640
rabah babaci <4rabah@gmail.com>
2020-08-11 12:39:09 -0700

    implement the first pass solution for single number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,14 +1,24 @@
-'''
+"""
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
-'''
+"""
+
+
 def single_number(arr):
     # Your code here
+    # since there is only one number that shows once
+    # we can loop over the list, and check each value to see
+    # if it's not repeated.
+    for i in range(len(arr)):
+        count = 0
+        for j in range(len(arr)):
+            if arr[i] == arr[j]:
+                count += 1
+        if count == 1:
+            return arr[i]
 
-    pass
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
