d06ee951c6ced159dd63ec0122dc3837db6644cb
rabah babaci <4rabah@gmail.com>
2020-08-11 13:04:20 -0700

    implement the first pass solution of moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,14 +1,24 @@
-'''
+"""
 Input: a List of integers
 Returns: a List of integers
-'''
+"""
+
+
 def moving_zeroes(arr):
-    # Your code here
+    # iterate over the array
+    # if 0 is found, shift all numbers to the left
+    # move the 0 to the end of the array
+    for i in range(len(arr) - 1, -1, -1):
+        if arr[i] == 0:
+            j = i
+            while j < len(arr) - 1:
+                arr[j] = arr[j + 1]
+                j += 1
+            arr[j] = 0
+    return arr
 
-    pass
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Use the main function here to test out your implementation
     arr = [0, 3, 1, 0, -2]
 
