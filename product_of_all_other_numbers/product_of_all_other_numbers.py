04b465243f06468f5dbc192b46fb21f62d83b2c3
rabah babaci <4rabah@gmail.com>
2020-08-11 13:19:20 -0700

    implement the first pass solution of product of all other numbers

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -1,16 +1,80 @@
-'''
+"""
 Input: a List of integers
 Returns: a List of integers
-'''
+"""
+
+
 def product_of_all_other_numbers(arr):
-    # Your code here
+    # loop over the array starting from index 0 always
+    # computing the product of all the values excluding the one
+    # at the current index, then append the result to the products array
+    products = []
+    for i in range(len(arr)):
+        result = 1
+        for j in range(len(arr)):
+            if j != i:
+                result *= arr[j]
+        products.append(result)
+    return products
 
-    pass
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Use the main function to test your implementation
     # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [
+        2,
+        6,
+        9,
+        8,
+        2,
+        2,
+        9,
+        10,
+        7,
+        4,
+        7,
+        1,
+        9,
+        5,
+        9,
+        1,
+        8,
+        1,
+        8,
+        6,
+        2,
+        6,
+        4,
+        8,
+        9,
+        5,
+        4,
+        9,
+        10,
+        3,
+        9,
+        1,
+        9,
+        2,
+        6,
+        8,
+        5,
+        5,
+        4,
+        7,
+        7,
+        5,
+        8,
+        1,
+        6,
+        5,
+        1,
+        7,
+        7,
+        8,
+    ]
 
-    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+    print(
+        f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}"
+    )
+
