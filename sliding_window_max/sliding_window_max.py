a2b55ea4d5df90feb7c956420d4aa1c339a2d72f
rabah babaci <4rabah@gmail.com>
2020-08-11 13:50:36 -0700

    implement the first pass solution of product of sliding window max

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,15 +1,27 @@
-'''
+"""
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
-'''
-def sliding_window_max(nums, k):
-    # Your code here
-
-    pass
+"""
 
 
-if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+def sliding_window_max(numbers, k):
+    # loop over the numbers till len(numbers) - k to avoid accessing out of range
+    # pick the max number in that size window
+    result = []
+    for i in range(len(numbers) - k + 1):
+        j = i
+        max_value = numbers[j]
+
+        while j < k + i:
+            if numbers[j] > max_value:
+                max_value = numbers[j]
+            j += 1
+        result.append(max_value)
+    return result
+
+
+if __name__ == "__main__":
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
