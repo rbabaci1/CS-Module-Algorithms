6ba1ffb13ef721ba533655af45f3d1cea98e2aef
rabah babaci <4rabah@gmail.com>
2020-08-12 10:12:25 -0700

    implement the eating cookies function

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,15 +2,30 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  # Your code here
 
-  pass
+def rock_paper_scissors(n):
+    options = ["rock", "paper", "scissors"]
+    result = []
+
+    if n == 0:
+        return [[]]
+    elif n == 1:
+        return [["rock"], ["paper"], ["scissors"]]
+    else:
+        # the length of the result will be always 3^n
+        # each sub array will always have length of n
+        for i in range(pow(3, n)):
+            sub = []
+            for j in range(n):
+                pass
+
+    return result
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print("Usage: rps.py [num_plays]")
+
