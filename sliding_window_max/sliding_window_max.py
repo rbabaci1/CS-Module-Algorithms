15d71f782883e00ff38bf3e1e1ea1ad08a6c31a2
rabah babaci <4rabah@gmail.com>
2020-08-14 12:58:17 -0700

    refactor product of all numbers

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,27 +2,33 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 """
+from collections import deque
 
 
-def sliding_window_max(numbers, k):
-    # loop over the numbers till len(numbers) - k to avoid accessing out of range
-    # pick the max number in that size window
+def sliding_window_max(arr, k):
+    Q = deque()
     result = []
-    for i in range(len(numbers) - k + 1):
-        j = i
-        max_value = numbers[j]
 
-        while j < k + i:
-            if numbers[j] > max_value:
-                max_value = numbers[j]
-            j += 1
-        result.append(max_value)
+    for i in range(k):
+        while Q and arr[i] >= arr[Q[-1]]:
+            Q.pop()
+        Q.append(i)
+
+    for i in range(k, len(arr)):
+        result.append(arr[Q[0]])
+        while Q and Q[0] <= i - k:
+            Q.popleft()
+
+        while Q and arr[i] >= arr[Q[-1]]:
+            Q.pop()
+        Q.append(i)
+    result.append(arr[Q[0]])
     return result
 
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    arr = [1, 3, -1, -3, 5, 3, 6, 7]
+    arr = [12, 1, 78, 90, 57, 89, 56]
     k = 3
 
     print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
